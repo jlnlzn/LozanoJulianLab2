7c1fb14702e83c605eec54d34ce16f0e56263f79
jlnlzn <42483441+jlnlzn@users.noreply.github.com>
2018-09-11 17:54:15 +0800

    Initial commit

---
+++ app/src/main/java/lozano/julian/com/lozanojulianlab2/Constraint.java
@@ -0,0 +1,14 @@
+package lozano.julian.com.lozanojulianlab2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Constraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen2);
+    }
+}
